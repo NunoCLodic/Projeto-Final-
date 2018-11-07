ae93966cc54857d5168864eee061e480c7a31648
NunoClodic Carvalho Lima <32763809+NunoCLodic@users.noreply.github.com>
2018-11-07 01:46:46 -0200

    Commit4(Site)
    ADD classe Estudante

---
+++ IPGUnidos/Models/Estudante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPGUnidos.Models
+{
+    public class Estudante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+        public string Pais { get; set; }
+        public int Escola { get; set; }
+    }
+}
