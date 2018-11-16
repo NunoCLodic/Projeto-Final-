cf694dddc4fae38cc3595cd1bab17a3ee313ed1f
NunoClodic Carvalho Lima <32763809+NunoCLodic@users.noreply.github.com>
2018-11-16 01:00:43 -0200

    Commit6 (UML)
    Diagrama de Classe
    Diagrama de Caso de Uso

--- IPGUnidos/Models/Usuario.cs
+++ IPGUnidos/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,25 @@ namespace IPGUnidos.Models
     public class Usuario
     {
         public int usuarioID{ get; set; }
+
+        [Required(ErrorMessage = "Por Favor Insira seu Nome")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por Favor Insira seu Telefone")]
         public int Telefone { get; set; }
+
+        [Required(ErrorMessage = "Por Favor Insira seu País")]
         public string Pais { get; set; }
+
+        [Required(ErrorMessage = "Por Favor Insira a sua Morada")]
         public int Morada { get; set; }
+
+        [Required(ErrorMessage = "Por Favor Insira seu Email")]
+        [RegularExpression(".+\\@.+\\..+",
+        ErrorMessage = "Por Favor Insira um Email Valido")]
         public int Email { get; set; }
+
+        [Required(ErrorMessage = "Por Favor Insira sua Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
     }
 }
